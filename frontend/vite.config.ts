11a6b8c5a637d41fbb3f702d35a96975b5d4c901
Your Name <raidenshogungenshin60@gmail.com>
2026-05-31 04:37:48 +0530

    errors...

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({mode}) => {
     server: {
       proxy: {
         '/api': {
-          target: 'http://127.0.0.1:8000',
+          target: env.VITE_API_URL || 'http://127.0.0.1:8000',
           changeOrigin: true,
         },
       },
